d8597487685b8e9b9d4e52d1a5fa05d4875052a4
EyE PunE <info@eyepune.com>
2026-06-10 01:20:25 +0530

    Final Google indexing fix and hydration resolution

--- push_fixes.bat
+++ push_fixes.bat
@@ -1,5 +1,5 @@
-git rebase --abort
 git add .
-git commit -m "Fix Googlebot traps and SEO Sitemap"
+git commit -m "Final Google indexing fix and hydration resolution"
+git pull origin main --rebase
 git push
 pause
